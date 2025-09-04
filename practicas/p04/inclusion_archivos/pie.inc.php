d9826cc55d827626d7adb9dfc5c5b4f35c0b6686
Carmonat <eduardoect21@gmail.com>
2025-09-03 20:19:56 -0600

    Correccion a algunos archivos

--- practicas/p04/inclusion_archivos/pie.inc.php
+++ practicas/p04/inclusion_archivos/pie.inc.php
@@ -1,21 +1,8 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>Página PHP</title>
-</head>
-<body>
-    <hr/>
-    <?php
-    echo "<div><h1 style=\"border-width:3px; border-style:groove; background-color:#ffcc99;\"> 
-    Final de la página PHP Vínculos útiles : 
-    <a href=\"php.net\">php.net</a>
-    &nbsp; 
-    <a href=\"mysql.org\">mysql.org</a>
-    </h1></div>";
-
-    echo "<div>Nombre del archivo ejecutado: " . $_SERVER['PHP_SELF'] . "&nbsp;&nbsp;&nbsp;";
-    echo "Nombre del archivo incluido: " . __FILE__ . "</div>";
-    ?>
-</body>
-</html>
+<hr/>
+<?php
+    echo "<div><h1 style=\"border-width:3;border-style:groove; background-color:
+    #ffcc99;\"> Final de la página PHP Vínculos útiles : <a href=\"php.net\">php.net</a>
+    &nbsp; <a href=\"mysql.org\">mysql.org</a></h1>";
+    echo "Nombre del archivo ejecutado: ", $_SERVER['PHP_SELF'],"&nbsp;&nbsp; &nbsp;";
+    echo "Nombre del archivo incluido: ", __FILE__ ,"</div>";
+?>
