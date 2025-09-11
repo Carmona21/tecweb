2084c93024527c99f6a036502d3596eb66b5783d
Carmonat <eduardoect21@gmail.com>
2025-09-10 18:40:08 -0600

    Ejercicio 1 y 3

---
+++ practicas/p05/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Práctica 3</title>
+</head>
+<body>
+    <h2>Ejercicio 1</h2>
+    <p>Determina cuál de las siguientes variables son válidas y explica por qué:</p>
+    <p>$_myvar,  $_7var,  myvar,  $myvar,  $var7,  $_element1, $house*5</p>
+    <?php
+        //AQUI VA MI CÓDIGO PHP
+        $_myvar;
+        $_7var;
+        //myvar;       // Inválida
+        $myvar;
+        $var7;
+        $_element1;
+        //$house*5;     // Invalida
+        
+        echo '<h4>Respuesta:</h4>';   
+    
+        echo '<ul>';
+        echo '<li>$_myvar es válida porque inicia con guión bajo.</li>';
+        echo '<li>$_7var es válida porque inicia con guión bajo.</li>';
+        echo '<li>myvar es inválida porque no tiene el signo de dolar ($).</li>';
+        echo '<li>$myvar es válida porque inicia con una letra.</li>';
+        echo '<li>$var7 es válida porque inicia con una letra.</li>';
+        echo '<li>$_element1 es válida porque inicia con guión bajo.</li>';
+        echo '<li>$house*5 es inválida porque el símbolo * no está permitido.</li>';
+        echo '</ul>';
+    ?>
+    <h2>Ejercicio 2</h2>
+    <p>Proporcionar los valores de $a, $b, $c como sigue:</p>
+    <p>$a = “ManejadorSQL”;,  $b = 'MySQL’;,  $c = &$a;</p>
+    <?php
+        // Asignaciones iniciales
+        $a = "ManejadorSQL";
+        $b = "MySQL";
+        $c = &$a;
+
+        echo '<h4>Respuesta:</h4>';
+        echo '<ul>';
+        echo "<li>Antes del segundo bloque de asignacion:</li>";
+        echo "<ul>";
+        echo "<li>a: $a</li>";
+        echo "<li>b: $b</li>";
+        echo "<li>c: $c</li>";
+        echo "</ul>";
+
+        $a = "PHP server";
+        $b = &$a;
+
+        echo "<li>Después del segundo bloque asignacion:</li>";
+        echo "<ul>";
+        echo "<li>a: $a</li>";
+        echo "<li>b: $b</li>";
+        echo "<li>c: $c</li>";
+        echo "</ul>";
+        echo "</ul>";
+
+        echo "<p><strong>Explicación:</strong> 
+        Primero <code>\$c</code> apuntaba a <code>\$a</code>, 
+        mientras que <code>\$b</code> era independiente. 
+        Después, al hacer <code>\$b =& \$a</code>, 
+        las tres variables quedaron referenciando el mismo valor y al actualizar el valor de
+        a estos tres cambiaron su valor.</p>";
+    ?>
+
+</body>
+</html>
