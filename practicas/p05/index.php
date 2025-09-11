c02cb963306c75abc536061be7de5bfc106c0158
Carmonat <eduardoect21@gmail.com>
2025-09-11 14:31:04 -0600

    Ejercicio 3 y 4 Comppletados, el anterior era 1 y 2

--- practicas/p05/index.php
+++ practicas/p05/index.php
@@ -32,40 +32,106 @@
     ?>
     <h2>Ejercicio 2</h2>
     <p>Proporcionar los valores de $a, $b, $c como sigue:</p>
-    <p>$a = “ManejadorSQL”;,  $b = 'MySQL’;,  $c = &$a;</p>
+    <p>$a = “ManejadorSQL”,  $b = 'MySQL’,  $c = &$a</p>
     <?php
-        // Asignaciones iniciales
         $a = "ManejadorSQL";
-        $b = "MySQL";
+        $b = 'MySQL';
         $c = &$a;
 
-        echo '<h4>Respuesta:</h4>';
         echo '<ul>';
-        echo "<li>Antes del segundo bloque de asignacion:</li>";
-        echo "<ul>";
-        echo "<li>a: $a</li>";
-        echo "<li>b: $b</li>";
-        echo "<li>c: $c</li>";
-        echo "</ul>";
+        echo '<li>$a es </li>', $a;
+        echo '<li>$b es </li>', $b;
+        echo '<li>$c es </li>', $c;
+        echo '</ul>';
 
-        $a = "PHP server";
-        $b = &$a;
-
-        echo "<li>Después del segundo bloque asignacion:</li>";
-        echo "<ul>";
-        echo "<li>a: $a</li>";
-        echo "<li>b: $b</li>";
-        echo "<li>c: $c</li>";
-        echo "</ul>";
-        echo "</ul>";
-
-        echo "<p><strong>Explicación:</strong> 
+        echo '<p><strong>Explicación:</strong> 
         Primero <code>\$c</code> apuntaba a <code>\$a</code>, 
         mientras que <code>\$b</code> era independiente. 
-        Después, al hacer <code>\$b =& \$a</code>, 
+        Después, al hacer <code>\$b = &\$a</code>, 
         las tres variables quedaron referenciando el mismo valor y al actualizar el valor de
-        a estos tres cambiaron su valor.</p>";
+        a estos tres cambiaron su valor.</p>';
+        $a = "PHP Server";
+        $b = &$a;
+        echo '<ul>';
+        echo '<li>$a es </li>', $a;
+        echo '<li>$b es </li>', $b;
+        echo '</ul>';
     ?>
+    <h2>Ejercicio 3</h2>
+    <p>Muestra el contenido de cada variable inmediatamente después de cada asignación,
+    verificar la evolución del tipo de estas variables (imprime todos los componentes de los
+    arreglo):</p>
+    <p>$a = “PHP5”, $z[] = &$a,  $b = “5a version de PHP”, $c = $b*10, $a .= $b,
+        $b *= $c, $z[0] = “MySQL”
+    </p>
+    <?php
+        unset($a);
+        unset($b);
+        unset($c);
+        $a = "PHP5";
+        echo "La variable a es: ", $a;
+        echo "<br><br>";
 
+        $z[] = &$a;
+        echo "La variable z es: ";
+        print_r($z);
+        echo "<br><br>";
+
+        $b = "5a version de PHP";
+        echo "La variable b es: ", $b;
+        echo "<br><br>";
+
+        @$c = $b*10;
+        echo "La variable c es: ", $c;
+        echo "<br><br>";
+
+        $a .= $b;
+        echo "La variable a es: ", $a;
+        echo "<br>La variable b es: ", $b;
+        echo "<br><br>";
+
+        @$b *= $c;
+        echo "La variable b es: ", $b;
+        echo "<br>La variable c es: ", $c;
+        echo "<br><br>";
+
+        $z[0] = "MySQL";
+        echo "La variable z es: ";
+        print_r($z);
+        echo "<br><br>";
+    ?>
+    <h2>Ejercicio 4</h2>
+    <p>Lee y muestra los valores de las variables del ejercicio anterior, pero ahora con la ayuda de
+    la matriz $GLOBALS o del modificador global de PHP.</p>
+    <?php
+        unset($a);
+        unset($b);
+        unset($c);
+        $a = "PHP5";
+        echo "La variable a = " . $GLOBALS["a"] . "<br><br>";
+
+        $z[] = &$a;
+        echo "La variable z = ";
+        print_r($GLOBALS["z"]);
+        echo "<br><br>";
+
+        $b = "5a version de PHP";
+        echo "La variable b = " . $GLOBALS["b"] . "<br><br>";
+
+        @$c = $b * 10;
+        echo "La variable c = " . $GLOBALS["c"] . "<br><br>";
+
+        $a .= $b;
+        echo "La variable a = " . $GLOBALS["a"] . "<br>";
+        echo "La variable b = " . $GLOBALS["b"] . "<br><br>";
+
+        @$b *= $c;
+        echo "La variable b = " . $GLOBALS["b"] . "<br>";
+        echo "La variable c = " . $GLOBALS["c"] . "<br><br>";
+
+        $z[0] = "MySQL";
+        echo "La variable z = ";
+        print_r($GLOBALS["z"]);
+    ?>
 </body>
 </html>
