e838bf8c2814d2aec84155f14d2c287f5c1d81cc
Carmonat <eduardoect21@gmail.com>
2025-09-25 10:15:51 -0600

    Ejercicio 2 completado

--- practicas/p07/src/funciones.php
+++ practicas/p07/src/funciones.php
@@ -10,4 +10,25 @@
             echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
         }
     }
+
+    function ejercicio2(){
+        $m = 0;
+        $matriz = [];
+        do{
+            
+            $num = mt_rand(0,1000);
+            $num2 = mt_rand(0,1000);
+            $num3 = mt_rand(0,1000);
+            $m++;
+            
+            $matriz[$m-1][0] = $num;
+            $matriz[$m-1][1] = $num2;
+            $matriz[$m-1][2] = $num3;
+            
+            
+        }while(!((($num % 2) == 1) && (($num2 % 2) == 0 ) && (($num3 % 2) == 1 )));
+
+        echo $num," impar ",$num2," par ",$num3," impar","<br>";
+        echo $m*3," numeros obtenidos en ",$m," iteraciones";
+    }
 ?>
