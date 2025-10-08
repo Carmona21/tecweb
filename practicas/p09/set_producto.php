2fd40768c7bb4956b5d6c762ae6db20a865afc0b
Carmonat <eduardoect21@gmail.com>
2025-10-08 12:38:51 -0600

    Ejercicio 1 completo

--- practicas/p09/set_producto.php
+++ practicas/p09/set_producto.php
@@ -8,7 +8,7 @@ $unidades = 1;
 $imagen   = 'img/imagen.png';
 
 /** SE CREA EL OBJETO DE CONEXION */
-@$link = new mysqli('localhost', 'root', '12345678a', 'marketzone');	
+@$link = new mysqli('localhost', 'root', 'pesadilla123', 'marketzone', 3307);	
 
 /** comprobar la conexión */
 if ($link->connect_errno) 
