dea6c0abcbba46f3730972ed588e12c34eb1983a
Carmonat <eduardoect21@gmail.com>
2025-11-24 19:19:55 -0600

    Uso de autoload y ya

--- practicas/p13/backend/product-add.php
+++ practicas/p13/backend/product-add.php
@@ -1,8 +1,9 @@
 <?php
-    use TECWEB\MYAPI\Products;
-    require_once __DIR__.'/myapi/Products.php';
+    use TECWEB\MYAPI\Create\Create;
+    require_once __DIR__ . '/vendor/autoload.php';
 
-    $productos = new Products('marketzone');
+    // "marketzone" es el nombre de tu base de datos
+    $productos = new Create('marketzone');
     $productos->add( json_decode( json_encode($_POST) ) );
     echo $productos->getData();
 ?>
